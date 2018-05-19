d749b62e5deb9afa567c10cb63f25890481443f4
BuildTools <unconfigured@null.spigotmc.org>
2018-05-18 21:30:42 -0700

    Initial commit

---
+++ actors.py
@@ -0,0 +1,57 @@
+import collections
+import random
+
+
+class Creature:
+    def __init__(self, name: str, level: int, attack_type: str):
+        self.name = name
+        self.level = level
+        self.attack_type = attack_type
+
+    def __repr__(self):
+        return "Creature: {} of level {} with attack_type: {}"\
+            .format(self.name, self.level, self.attack_type)
+
+    def get_defensive_roll(self):
+        return self.level * random.randint(1, 12)
+
+class Hero(Creature):
+    def defeat(self, target):
+        attack_strength = self.get_defensive_roll()
+        target_strength = target.get_defensive_roll()
+        print('You attack with {} damage...'.format(attack_strength))
+        print('{} {}s with {} damage...'.format(target.name, target.attack_type, target_strength))
+        return attack_strength >= target_strength
+
+
+class Dog(Creature):
+    def __init__(self, name: str, level: int, attack_type: str, crazy: bool):
+        name_desc = 'crazy {}'.format(name) if crazy else name
+        self.name = name_desc
+        self.level = level
+        self.attack_type = attack_type
+        self.crazy = crazy
+
+    def get_defensive_roll(self):
+        crazy_mod = 10 if self.crazy else 1
+        return super().get_defensive_roll() * crazy_mod
+
+
+class Parent(Creature):
+    def __init__(self, name: str, level: int, attack_type: str, mood):
+        self.mood = mood
+        self.mood_val = collections.namedtuple('moods', ['angry', 'tired', 'happy'])(5, 3, 1)
+        self.name = '{} {}'.format(self.mood_val._fields[self.mood], name)
+        self.level = level
+        self.attack_type = attack_type
+
+
+
+    def get_defensive_roll(self):
+
+        # Use this assignment if you pass mood string in initializer
+        # moody_mod = getattr(self.mood_val, self.mood)
+
+        # Use this assignment if you pass index in initializer
+        moody_mod = self.mood_val[self.mood]
+        return super().get_defensive_roll()*moody_mod
---
+++ program.py
@@ -0,0 +1,80 @@
+import random
+import time
+import actors
+
+
+def print_header():
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('                      FAMILY BRAWL')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('')
+
+
+def game_loop(name: str):
+    creatures = [
+        actors.Creature('Darth', random.randint(2, 4), 'claw'),
+        actors.Creature("Ghost of Gizmo", random.randint(2, 4), 'slime'),
+        actors.Dog('Delilah', random.randint(2, 5), 'fart', (random.randint(1, 10) > 8)),
+        # actors.Parent('Mom', random.randint(3, 7), 'nag', 'angry'),
+        actors.Parent('Mom', random.randint(3, 7), 'nag', random.randint(0, 2)),
+        # actors.Parent('Dad', random.randint(5, 10), 'burp', 'tired')
+        actors.Parent('Dad', random.randint(5, 10), 'burp', random.randint(0, 2))
+    ]
+
+    player = actors.Hero(name, random.randint(3, 20), 'nerf gun')
+    lives = 3
+
+    print('\n{} was supposed to do homework, '
+          'but went out front to play with friends instead.'
+          .format(name))
+    while True:
+
+        active_creature = random.choice(creatures)
+
+        print('\n{} of level {} has cornered {} in the kitchen...'
+              .format(active_creature.name.title(), active_creature.level, name))
+
+        cmd = input('{}, do you want to [a]ttack, [r]unaway, or [l]ook around? '
+                    .format(name)).lower()
+
+        if cmd == 'a':
+            print('\n{} attacks {}!'.format(name, active_creature.name))
+            if player.defeat(active_creature):
+                print('{} has defeated {}'.format(name, active_creature.name))
+                creatures.remove(active_creature)
+            else:
+                print('{} is defeated by {}!'.format(name, active_creature.name))
+                lives -= 1
+                print('{} has {} lives remaining'.format(name, lives))
+                if lives <= 0:
+                    print('\n{} is DEAD! Goodbye.'.format(name))
+                    break
+
+        elif cmd == 'r':
+            print('{} has to poop and runs to the bathroom...'.format(name))
+            time.sleep(3)
+            print('{} is done pooping and returns feeling better than ever.'.format(name))
+
+        elif cmd == 'l':
+            print('\n{} looks around and sees:'.format(name))
+            for c in creatures:
+                print(' * {} (level: {})'.format(c.name.title(), c.level))
+
+        else:
+            quitter = input('\nDo you really want to quit like a [q]uiting quitter? ')
+            if quitter.lower() in ['y', 'x', 'q']:
+                print('\n{} has quit Family Brawl...\nYou LOOSE!'.format(name))
+                break
+            else:
+                print("I knew you weren't a quitting quitter!")
+
+
+def main():
+    print_header()
+    hero_name = input('What is your name? ').title()
+
+    game_loop(hero_name)
+
+
+if __name__ == '__main__':
+    main()
